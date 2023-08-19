855853831bd006c9b71f2b450739bdd4fad6b01e
Piyush Assudani <63794264+assudani66@users.noreply.github.com>
2023-08-19 15:56:53 +0530

    prettier changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,29 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
-import { NextRequest } from 'next/server'
+import { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
-  const {data}  = await supabase.auth.getSession()
-  const userDetails = await supabase.from('profiles').select('username').eq('id',data?.session?.user?.id).single()
-  
-  if(!userDetails?.data?.username && data.session ){
-    return NextResponse.redirect(new URL('/profile/edit',req.nextUrl))
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  const { data } = await supabase.auth.getSession();
+  const userDetails = await supabase
+    .from("profiles")
+    .select("username")
+    .eq("id", data?.session?.user?.id)
+    .single();
+
+  if (!userDetails?.data?.username && data.session) {
+    return NextResponse.redirect(new URL("/profile/edit", req.nextUrl));
   }
 
-  if(!data.session){
-    return NextResponse.redirect(new URL('/auth',req.nextUrl))
+  if (!data.session) {
+    return NextResponse.redirect(new URL("/auth", req.nextUrl));
   }
-  return res
+  return res;
 }
 
 export const config = {
-  matcher:[
-    '/',
-    '/profile',
-    '/create'
-]
-}
+  matcher: ["/", "/profile", "/create"],
+};
